0d50142e816b5ac0d9601321420efe26d9474e27
SpyTec <spytec13@gmail.com>
2015-06-20 20:27:47 +0200

    Added Explorer ranks, added functionality of subcategories
    Subcategories can now be placed inside categories to link to other data
    files

--- js/controllers/AssetTypes.js
+++ js/controllers/AssetTypes.js
@@ -8,6 +8,8 @@ app.controller('AssetType', ['$http','$stateParams', '$scope', 'type', function(
     $http.get(unescape($stateParams.file)) 
             .success(function(data) { 
                 $scope.category = data[0]['name'];
+                $scope.issubcat = data[0]['issubcat'];
+                $scope.subcats = data[0]['subcats'];
                 $scope.typeAssets = data[0]['types'];
             }); 
     /*type($stateParams.file).query().success(function(data) {

0dd2c2f417aec2cd152117c8a90b6377ece20371
Frank Boës <frank.boes@shift.agency>
2016-01-19 09:05:50 +0100

    Fixed filenames

--- fonts/get-svg.sh
+++ fonts/get-svg.sh
@@ -2,5 +2,8 @@
 
 set -e
 cd ${0%/*}/..
+mkdir -p fonts/elite-icons/source
 find img -type f -name \*.svg -exec cp -p {} fonts/elite-icons/source \;
+for f in fonts/elite-icons/source/*.svg; do mv "$f" "${f// /_}"; done
+for f in fonts/elite-icons/source/*.svg; do mv $f `echo $f | tr [:upper:] [:lower:]`; done
 find fonts/elite-icons/source -type f -name \*.svg -exec sed -i '' 's/\#[A-Za-z0-9]*/#000000/g' {} \;

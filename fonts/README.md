1a49dbf31f7937a0dde360629017eec6d5fe476b
Frank Boës <frank.boes@shift.agency>
2016-01-19 09:55:44 +0100

    Adding explorer icons as well as README

---
+++ fonts/README.md
@@ -0,0 +1,12 @@
+# How to build the icon font
+
+1. Run `./get.svg.sh`. This will copy all SVGs files to [`elite-icons/source`](elite-icons/source/)
+2. Open up https://icomoon.io/app
+2. If it is your first time in IcoMoon, select "Import Icons" and load [`elite-icons/source/selection.json`](elite-icons/source/selection.json) into IcoMoon App
+3. Click right on the burger menu and select "Import to set"
+4. Select all SVGs
+5. Select "Replace"
+6. Click on the burger menu and select "Select All"
+7. Click on "Generate font" in the lower right corner
+8. Click "Download" in the lower right corner
+9. Copy contents of ZIP to [`elite-icons`](elite-icons/)
--- fonts/get-svg.sh
+++ fonts/get-svg.sh
@@ -2,8 +2,13 @@
 
 set -e
 cd ${0%/*}/..
+rm -rf fonts/elite-icons/source
 mkdir -p fonts/elite-icons/source
 find img -type f -name \*.svg -exec cp -p {} fonts/elite-icons/source \;
-for f in fonts/elite-icons/source/*.svg; do mv "$f" "${f// /_}"; done
-for f in fonts/elite-icons/source/*.svg; do mv $f `echo $f | tr [:upper:] [:lower:]`; done
+for f in img/pilots-federation/explorer/*.svg; do
+	cp "$f" fonts/elite-icons/source/${f//img\/pilots-federation\/explorer\/rank/rank-explorer}
+done
+for f in fonts/elite-icons/source/*.svg; do
+	mv "$f" `echo "${f// /_}" | tr [:upper:] [:lower:]`
+done
 find fonts/elite-icons/source -type f -name \*.svg -exec sed -i '' 's/\#[A-Za-z0-9]*/#000000/g' {} \;
